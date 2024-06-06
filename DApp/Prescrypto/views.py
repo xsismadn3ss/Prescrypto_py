50e8bc4b49f94054d52f81d0b5ce34dde1656b43
xsismadn3ss <2477carlos.artiga@gmail.com>
2024-06-06 10:03:31 -0600

    crear vistas principales para la aplicación

--- DApp/Prescrypto/views.py
+++ DApp/Prescrypto/views.py
@@ -1,3 +1,25 @@
+from re import template
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from Prescrypto.auth_model import auth
+
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html", {"home_data:":auth.description, "auth": auth.state})
+
+def prescriptions(request):
+    template = loader.get_template("prescriptions.html")
+    return HttpResponse(template.render())
+
+def recetas(reques):
+    template = loader.get_template("recetas.html")
+    return HttpResponse(template.render())
+
+def test(request):
+    template = loader.get_template("myfirst.html")
+    return HttpResponse(template.render())
+
+def memebers(request):
+    return HttpResponse("hello world")
