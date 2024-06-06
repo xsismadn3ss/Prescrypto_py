fe2030bc1e1b62561be727fa78d15c1238d901ba
xsismadn3ss <2477carlos.artiga@gmail.com>
2024-06-06 10:01:05 -0600

    dataclasses para hacer formularios

---
+++ DApp/Prescrypto/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from logging import PlaceHolder
+from django import forms
+from .models import Users
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Users
+        fields = '__all__'
