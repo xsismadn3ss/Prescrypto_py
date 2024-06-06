4131100470505aec977406701aad0e5446004552
xsismadn3ss <2477carlos.artiga@gmail.com>
2024-06-06 10:01:29 -0600

    anexar urls de las vistas

---
+++ DApp/Prescrypto/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .accounts import views as account_view
+from . import views
+
+
+urlpatterns = [
+
+    #contenido
+    path('', view=views.home, name='home'),
+    path('recetas',view=views.recetas, name='recetas'),
+    path('prescriptions/', view=views.prescriptions, name="prescriptions"),
+    
+
+    #cuentas
+    path('login/', view=account_view.login, name='login'),
+    path('logout/', view=account_view.logout, name='logout'),
+    path('sign_up/', view=account_view.create_account, name="sign_up"),
+
+    #test
+    path('test/', view=views.test, name='test'),
+    path('members/', views.memebers, name='members'),
+]
