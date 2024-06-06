759a9f5c842e73b94833497ff2a278315f84c64e
xsismadn3ss <2477carlos.artiga@gmail.com>
2024-06-06 09:58:18 -0600

    hacer migraciones de cambios hechos en los modelos de la base de datos

--- DApp/Prescrypto/models.py
+++ DApp/Prescrypto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    #campos: nombre, apellido, dui, wallet, pk, password
+    firstname = models.CharField(max_length=50, verbose_name="Nombres")
+    lastname = models.CharField(max_length=50, verbose_name="Apellidos")
+    username = models.CharField(max_length=50, verbose_name="Username", unique=True)
+    dui = models.CharField(max_length=50, verbose_name="DUI", unique=True, default="")
+    wallet = models.CharField(max_length=500, verbose_name="Wallet", unique=True)
+    private_key = models.CharField(max_length=500, verbose_name="Private key", unique=True)
+    password = models.CharField(max_length=20, verbose_name="Contraseña")
