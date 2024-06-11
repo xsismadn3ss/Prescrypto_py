f316fcc370c92ff0d45efdeee71c168cc23680a6
xsismadn3ss <2477carlos.artiga@gmail.com>
2024-06-11 16:02:13 -0600

    agregar vista de detalles de cuenta

--- DApp/Prescrypto/auth_model.py
+++ DApp/Prescrypto/auth_model.py
@@ -3,9 +3,10 @@ class Auth():
     """Auth
        class to make auth object wich validate if the session is started or don't  
     """
-    def __init__(self, state:bool, description = "", user = None):
+    def __init__(self, state:bool, id, description = "", user = None):
+        self.id = id
         self.state = state
         self.description = description
         self.user = user
 
-auth = Auth(False)
+auth = Auth(False, None)
